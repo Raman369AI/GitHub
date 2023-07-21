0da70d4e53cd8d259ca0f9cbdda618ed06040a9b
Raman369AI <110351568+Raman369AI@users.noreply.github.com>
2023-07-21 20:07:19 +0530

    version @0.1.1

--- 100_days/stock_market/main.py
+++ 100_days/stock_market/main.py
@@ -1,13 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
-SBIN = yf.Ticker("SBIN.NS").history(period='1mo',interval='15m')
+def magnitude(x):
+    if x>0:
+        return 1
+    elif x==0:
+        return 0
+    else:
+        return -1
+
+SBIN = yf.Ticker("SBIN.NS").history(period='7d',interval='1m')
 SBIN=pd.DataFrame(SBIN)
 SBIN["direction"]=SBIN["Close"] - SBIN["Open"]
+#use apply() method to apply the defined function for each row of the selected column
+SBIN["direction"]=SBIN["direction"].apply(magnitude)
 SBIN = SBIN.dropna()
 SBIN = SBIN.round({"Close": 0})
 SBIN = SBIN.pivot_table(index=[SBIN["Close"]])
-SBIN["True_volume"] = SBIN["Volume"]*SBIN["direction"]
+
+SBIN["True_volume"] = (SBIN["Volume"])*SBIN["direction"]
 SBIN = SBIN.round({"Volume": 0})
 print(SBIN)
 plt.scatter(SBIN.index, SBIN["True_volume"])
