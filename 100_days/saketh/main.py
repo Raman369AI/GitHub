3528e1a0efb3506e8bfa4a4c1b8585a5946d0ef9
Raman369AI <110351568+Raman369AI@users.noreply.github.com>
2024-02-03 15:57:04 -0600

    Done
    Done

--- 100_days/saketh/main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import nltk
-saketh= pd.read_csv("123.csv")
-saketh=pd.DataFrame(saketh)
-saketh=saketh.dropna(axis=1, how="all")
-saketh.groupby('What are your Professional goals? This is all about you! Some ideas to think about: What do you want to professionally achieve in the next 5 years? Career movement or skill development? Flexible work arrangements, retirement. What does excite you about coming to work? What would excite you about coming to work?e.g., cross-unit project involvement To help you think about your professional goals, try using this tool. LINK| GROW MODEL	')
-print(saketh)
