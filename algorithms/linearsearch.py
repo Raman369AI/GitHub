ea55769f82af222e1d7b1806352fe59ee682291f
kronos <ramanraj33@gmail.com>
2024-05-30 12:32:22 -0600

    linear search algoirthm - brute force.

--- algorithms/linearsearch.py
+++ algorithms/linearsearch.py
@@ -1,19 +1,38 @@
 
 def locate_cards(cards,query):
-    if len(cards) == 0:
-        print('Deck is empty')
-    elif cards[0] == query:
-            print(0)
-    elif cards[-1] == query:
-            print(len(cards)-1)
+    position = 0
+    length = len(cards)
+    while True:
+        if  length == 0 or query == '':
+            return -1
+        elif cards[0] == query:
+                return 0
+        elif cards[-1] == query:
+                return (len(cards)-1)
+        elif position == length - 1:
+                return -1
+        elif cards[position] == query :
+            return position
+        position += 1
+            
+                
+            
+            
+            
+'''
+1. Check the cards length or if the query is empty then return a value of -1.
+2. Check the edge cases if the query is present on the first or last of the list and return the position
+3. Iterate over the length of the cards deck and return the position of the card if a match is 
+found or increment the position and redo the step3 until a match is found
+'''
+tests = []
+test = {'input':{'cards':[11],'query':''},'output':0}
+tests.append(test)
+tests.append({'input':{'cards':[11,13,14,14,15],'query':14},'output':2})
+''' test case for checking in  a coding round, using multiple cases to check the code'''
+'''using ** allows python interpreter to take the dictionary keys as inputs of the function'''
+for i in range(len(tests)):
+    if locate_cards(**tests[i]['input']) == tests[i]['output']:
+        print(f"Test case passed and position is {locate_cards(**tests[i]['input'])}")
     else:
-        for i in range(len(cards)):
-            if cards[i] == query:
-                print (i)
-    
-
-cards = [3,11,10,21]
-''' test case for checking in  a coding round'''
-#query = 7
-#output = 3
-locate_cards(cards,10)
+        print("Test case failed")
