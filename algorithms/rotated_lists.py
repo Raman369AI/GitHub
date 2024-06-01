495210c7e2fe6f43821d43808c25ddb735a543cf
kronos <ramanraj33@gmail.com>
2024-06-01 07:45:03 -0600

    updated the logic for most of the files

--- algorithms/rotated_lists.py
+++ algorithms/rotated_lists.py
@@ -3,18 +3,27 @@ Problem statement: Determine the minimum no of times a sorted list was rotated t
 '''
 import sys
 def rotated_count(list_orig,list_mod):
-    position = list_mod[0]
+    # Addressed the non existence of a list element, valueerror
+    if len(list_mod) != 0:
+        position = list_mod[0]
+    else:
+        position = 0
     initial = 0
     final_length = len(list_orig)
     length = len(list_orig)-1
+    if len(list_orig) != len(list_mod) or len(list_orig) == 0 or len(list_mod) == 0:
+        return -2
     while length >= initial:
         middle = (length + initial)//2
-        if position == list_orig[middle]:
+        if list_mod[0] == list_orig[0]:
+            return 0
+        elif position == list_orig[middle]:
             return int(final_length - middle)
         elif position > list_orig[middle]:
             length = middle - 1
         else:
             initial = middle + 1
+    return -1
 
 '''
 1. The modified list initial number is searched in the original list and the index of the modified list
@@ -23,8 +32,13 @@ Total length minus the previous index value of the first value in the modified l
 '''
 
 tests = []
+
+tests.append({'input':{'list_orig':[],'list_mod':[]},'output':-2})
+tests.append({'input':{'list_orig':[],'list_mod':[14, 56, 55, 54, 53, 52]},'output':-2})
 tests.append({'input':{'list_orig':[57, 56, 55, 54,  53, 52],'list_mod':[52,57, 56, 55,  54, 53]},'output':1})
 tests.append({'input':{'list_orig':[57, 56, 55, 54,  53, 52],'list_mod':[53, 52,57, 56,  55, 54]},'output':2})
+tests.append({'input':{'list_orig':[57, 56, 55, 54,  53, 52],'list_mod':[57, 56, 55, 54,  53, 52]},'output':0})
+tests.append({'input':{'list_orig':[57, 56, 55, 54,  53, 52],'list_mod':[14, 56, 55, 54,  53, 52]},'output':-1})
 
 
 
