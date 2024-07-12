e5a635b557b17b809f0994ab69711b89110f518c
kronos <ramanraj33@gmail.com>
2024-07-12 11:01:36 -0600

    all algorithms included

--- algorithms/practice.py
+++ algorithms/practice.py
@@ -5,10 +5,21 @@ sorting algorithms
 sor = [3, 2, 5, 1, 0]
 
 def bubblesort(lis):
-    for i in range(len(lis)-1):
-       for j in range(len(lis)-1):
+    h = 0
+    #for i in range(len(lis)-1):
+    for j in range(len(lis)-1):
         if lis[j]>lis[j+1] and j!=len(lis)-1:
             lis[j],lis[j+1] = lis[j+1],lis[j]
+            bubblesort(lis)
+            h = h+1
+            print(h)
     return lis
 print(bubblesort(sor))
+
+
+'''
+1. Append the first element
+2. Check with the minimum element and last element and push
+'''
+
         
